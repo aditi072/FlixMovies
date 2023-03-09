fdd8c78f149cb575a4fafda09d117e0f0f9c7fcb
Aditi <adittisharma072@gmail.com>
2023-03-09 16:02:10 +0530

    screenshots

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl : 'https://api.themoviedb.org/3/',
+  apiKey : 'dd4d819639705d332d531217b4f7c6b6',
+  language : 'en-US',
+  region : 'US'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl : 'https://api.themoviedb.org/3/',
+  apiKey : 'dd4d819639705d332d531217b4f7c6b6',
+  language : 'en-US',
+  region : 'US'
 };
 
 /*
